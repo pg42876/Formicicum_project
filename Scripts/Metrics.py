8e4e9b0a4b85aafe15e1ce20f31d131b0163d2c7
mari98 <pg42876@alunos.uminho.pt>
2022-07-01 14:51:48 +0100

    Metrics reactions

--- Scripts/Metrics.py
+++ Scripts/Metrics.py
@@ -1,28 +1,25 @@
 import pandas as pd
+from Scripts.utils import ReconstructionTool, get_reactions, get_genes, get_metabolites, calculate_quality_metrics
 
-carveme = pd.read_csv('../Results/carveme_uniprotinfo.tsv', sep ='\t')
-carveme_genes = carveme['Entry'].tolist()
-print(carveme_genes)
+with open(f'../Results/formicicum_reactions.txt', 'r') as f:
 
-formicicum = pd.read_csv('../Results/formicicum_uniprotinfo.tsv', sep ='\t')
-formicicum_genes = formicicum['Entry'].tolist()
-print(formicicum_genes)
+    formicicum = f.readlines()
 
-TPs = [ide for ide in carveme_genes if ide in formicicum_genes]
-FPs = [ide for ide in carveme_genes if ide not in formicicum_genes]
-FNs = [ide for ide in formicicum_genes if ide not in carveme_genes]
+    print(formicicum)
+    print(formicicum[0])
 
-print(len(TPs))
-print(len(FPs))
-print(len(FNs))
+    for reaction in formicicum:
+        print(reaction)
 
-def calculate_quality_metrics(tp, fp, fn):
-    precision = tp / (tp + fp)
-    recall = tp / (tp + fn)
-    f1_score = 2 * (precision * recall) / (precision + recall)
-    jaccard_distance = 1 - ( tp / fp + fp + fn)
-    return precision, recall, f1_score, jaccard_distance
+print(formicicum)
 
-print(calculate_quality_metrics(len(TPs), len(FPs), len(FNs)))
-
-CV = calculate_quality_metrics(len(TPs), len(FPs), len(FNs))
+metrics = []
+for tool in ['merlin_blast', 'merlin_bit', 'carveme', 'kbase']:
+    tool_reactions = open(f'../Results/kegg_functional_ids.tsv', 'r')
+    #reactions = tool_reactions['reactions']
+    TPs = len([ide for ide in tool_reactions if ide in formicicum])
+    FPs = len([ide for ide in tool_reactions if ide not in formicicum])
+    FNs = len([ide for ide in formicicum if ide not in tool_reactions])
+    print(TPs, FNs, FPs)
+    metrics.append([tool, TPs, FPs, FNs] + list(calculate_quality_metrics(TPs, FPs, FNs)))
+pd.DataFrame(metrics, columns=['tool', 'TPs', 'FPs', 'FNs', 'Precision', 'Recall', 'F1 score', 'Jaccard distance']).to_excel('../Results/quality_metrics_reactions.xlsx', index=False)
