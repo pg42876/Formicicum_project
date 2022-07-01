803e353b930d8df42dba603dd32498c32e03a696
mari98 <pg42876@alunos.uminho.pt>
2022-07-01 16:41:11 +0100

    Results reactions

--- Scripts/Metrics.py
+++ Scripts/Metrics.py
@@ -1,25 +1,25 @@
 import pandas as pd
 from Scripts.utils import ReconstructionTool, get_reactions, get_genes, get_metabolites, calculate_quality_metrics
 
-with open(f'../Results/formicicum_reactions.txt', 'r') as f:
+formicicum_metabolites = pd.read_csv('../Results/formicicum_metabolites.txt', sep='\n', header=None)[0].tolist()
+func_ids_df = pd.read_csv('../Results/metanetx_functional_ids.tsv', sep='\t', index_col=0)
 
-    formicicum = f.readlines()
+conversion = pd.read_csv('../Scripts/Xrefs files/compounds-conversion.csv')
 
-    print(formicicum)
-    print(formicicum[0])
+conversion['External ID'].isin(formicicum_metabolites).sum() #Verdadeiros
 
-    for reaction in formicicum:
-        print(reaction)
-
-print(formicicum)
+reactions = conversion[conversion['External ID'].isin(formicicum_reactions)]['Internal ID'].tolist()
+#print(reactions)
 
 metrics = []
-for tool in ['merlin_blast', 'merlin_bit', 'carveme', 'kbase']:
-    tool_reactions = open(f'../Results/kegg_functional_ids.tsv', 'r')
-    #reactions = tool_reactions['reactions']
-    TPs = len([ide for ide in tool_reactions if ide in formicicum])
-    FPs = len([ide for ide in tool_reactions if ide not in formicicum])
-    FNs = len([ide for ide in formicicum if ide not in tool_reactions])
-    print(TPs, FNs, FPs)
+for tool in ['aureme', 'merlin_blast', 'merlin_bit', 'carveme', 'kbase']:
+    func_reactions = func_ids_df['metabolites'].tolist()
+    lista = func_reactions[0].split(',')
+    print(lista)
+    TPs = len([ide for ide in lista if ide in reactions])
+    FPs = len([ide for ide in lista if ide not in reactions])
+    FNs = len([ide for ide in reactions if ide not in lista])
+    #print(TPs, FNs, FPs)
     metrics.append([tool, TPs, FPs, FNs] + list(calculate_quality_metrics(TPs, FPs, FNs)))
-pd.DataFrame(metrics, columns=['tool', 'TPs', 'FPs', 'FNs', 'Precision', 'Recall', 'F1 score', 'Jaccard distance']).to_excel('../Results/quality_metrics_reactions.xlsx', index=False)
+pd.DataFrame(metrics, columns=['tool', 'TPs', 'FPs', 'FNs', 'Precision', 'Recall', 'F1 score', 'Jaccard distance']).to_excel(
+    '../Results/quality_metrics_compounds.xlsx', index=False)
